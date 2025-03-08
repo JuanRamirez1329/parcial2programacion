b7c08522ee4501025063afa55b5706d17afc84d8
JuanRamirez1329 <juan1329david@gmail.com>
2025-03-07 22:38:03 -0500

    Add files via upload

---
+++ springboot_i18n/src/main/java/com/example/demo/controller/SaludoController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/api")
+public class SaludoController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/saludo")
+    public String obtenerSaludo(@RequestParam(name = "lang", required = false, defaultValue = "es") String lang) {
+        Locale locale = new Locale(lang);
+        return messageSource.getMessage("saludo", null, locale);
+    }
+}
